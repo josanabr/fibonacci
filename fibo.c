041ab2584002811ac33f6b80fc8a624c747fd2ac
John Sanabria <johnsanabria@correounivalle.edu.co>
2020-06-10 20:21:12 -0500

    primera version fibo

---
+++ fibo.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int fibo(int f) {
+  if (f == 1) return 0;
+  if (f == 2) return 1;
+  return fibo(f - 1) + fibo(f - 2);
+}
+
+int main(int argc, char** argv) {
+
+  printf("%d\n",fibo(5));
+
+  return 0; 
+}
